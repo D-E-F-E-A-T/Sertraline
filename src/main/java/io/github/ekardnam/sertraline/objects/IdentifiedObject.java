47578f02cbb5adb0488afd7910036710a3874aca
ekardnam <lucabertozzi.pub@gmail.com>
2018-03-04 10:20:14 +0100

    Ids for identified objects

--- src/main/java/io/github/ekardnam/sertraline/objects/IdentifiedObject.java
+++ src/main/java/io/github/ekardnam/sertraline/objects/IdentifiedObject.java
@@ -1,13 +1,26 @@
 package io.github.ekardnam.sertraline.objects;
 
-import java.util.Objects;
 
 public class IdentifiedObject {
 
-    @Override
-    public boolean equals(Object other) { return this == other; }
+    private static long currentId = 0;
+
+    //TODO("Handle overflow")
+    protected long id;
+
+    public IdentifiedObject() {
+        id = currentId;
+        currentId++;
+    }
 
     @Override
-    public int hashCode() { return Objects.hashCode(this); }
+    public boolean equals(Object other) {
+        if (!(other instanceof IdentifiedObject)) return false;
+        IdentifiedObject identified = (IdentifiedObject) other;
+        return id == identified.id;
+    }
+
+    @Override
+    public int hashCode() { return Long.hashCode(id); }
 
 }
